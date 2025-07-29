1b93149bdf6b4efdfaf2b95cf9ab2d065a3039b3
Hizkeal <104999321+hi-z-k@users.noreply.github.com.>
2025-07-29 11:09:35 +0300

    Implement function: RPSwinner
    - figures out which player won the single game or
    if it ended in a tie

--- JS/app.js
+++ JS/app.js
@@ -24,3 +24,20 @@ function getHumanChoice(){
     let choice = prompt("Choose to play(rock/paper/scissor): ")
     return choice
 }
+
+function RPSwinner(player1, player2){
+    let playerI = choiceTranslate(player1)
+    let playerII = choiceTranslate(player2)
+    let diff = Math.abs(playerII - playerI)
+
+    if (diff == 2){
+        return (playerI > playerII) ? 2 : 1
+    }
+    else if (diff == 1){
+        return (playerI > playerII) ? 1 : 2
+    }
+    else{
+        return 0;
+    }
+}
+
