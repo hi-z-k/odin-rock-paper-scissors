6e9296ea9bd455c0cdfa1b3c938947f352b94697
Hizkeal <104999321+hi-z-k@users.noreply.github.com.>
2025-08-20 10:56:41 +0300

    Partial Refactor the code to acheive
    - clean up the console version
    - preserve the game logic
    - manupilate the choice displayed with DOM
    - check validity of an option from event

--- JS/app.js
+++ JS/app.js
@@ -1,127 +1,6 @@
-function choiceTranslate(choice) {
-  let choiceList = ["rock", "paper", "scissor"];
-  if (typeof choice === "number") {
-    choice %= 3;
-    return choiceList[choice];
-  } else if (typeof choice === "string") {
-    choice = choice.toLowerCase();
-    return choiceList.indexOf(choice);
-  } else {
-    return;
-  }
-}
-
-function getComputerChoice() {
-  let choice = Math.floor(Math.random() * 4);
-  return choiceTranslate(choice);
-}
-
-function getHumanChoice() {
-  let choice = prompt("Choose to play(rock/paper/scissor): ");
-  return choice;
-}
-
-function RPSwinner(player1, player2) {
-  let playerI = choiceTranslate(player1);
-  let playerII = choiceTranslate(player2);
-  let diff = Math.abs(playerII - playerI);
-
-  if (diff == 2) {
-    return playerI > playerII ? 2 : 1;
-  } else if (diff == 1) {
-    return playerI > playerII ? 1 : 2;
-  } else if (diff == 0) {
-    return 0;
-  }
-}
-
-function playRound(player1, player2) {
-  console.log(`  (${player1} - ${player2}\)\n`);
-
-  let winner = RPSwinner(player1, player2);
-  if (winner == 1) {
-    console.log(`>> Player 1 wins this round!`);
-  } else if (winner == 2) {
-    console.log(`>> Player 2 wins this round!`);
-  } else {
-    console.log(">> It is a tie!");
-  }
-
-  return winner;
-}
-
-function scoreBoard(score1, score2) {
-  return `Player I (${score1} - ${score2}\) Player II`;
-}
-
-function RPSGame(bestOf) {
-  let pl1Score = 0;
-  let pl2Score = 0;
-  let round = 1;
-
-  while (round <= bestOf) {
-    console.log(
-      `\nRound ${round} - Scoreboard:\n  ${scoreBoard(pl1Score, pl2Score)}`
-    );
-    let winner = playRound();
-
-    if (winner === 1) pl1Score++;
-    else if (winner === 2) pl2Score++;
-    round++;
-  }
-
-  console.log(`\nFinal Results:\n  ${scoreBoard(pl1Score, pl2Score)}`);
-  if (pl1Score === pl2Score) {
-    console.log(">> It is a tie");
-  } else {
-    console.log(`>> The winner is Player ${pl1Score > pl2Score ? 1 : 2}`);
-  }
-}
-
-// RPSGame(3)
-function RPSuiGame(bestOf) {
-  const optionMenu = document.querySelector(".player-option");
-  let roundsLeft = bestOf;
-  const roundCount = document.querySelector(".round-count");
-  let round = 1;
-  roundCount.textContent = `Round ${round} of ${bestOf}`;
-  function RPSUI(e) {
-    let btnOption = e.target.getAttribute("class");
-    let userChoice = btnOption;
-    if (
-      userChoice == "rock" ||
-      userChoice == "paper" ||
-      userChoice == "scissor"
-    ) {
-      let computerChoice = getComputerChoice();
-      let winner = playRound(userChoice, computerChoice);
-
-      document.querySelector(".player-one.choice").textContent = userChoice;
-      document.querySelector(".player-two.choice").textContent = computerChoice;
-
-      alert(choiceTranslate(winner));
-      roundsLeft--;
-      if (roundsLeft === 0) {
-        optionMenu.removeEventListener("click", RPSUI);
-      }
-      if (roundsLeft > 0) round++;
-      roundCount.textContent = `Round ${round} of ${bestOf}`;
-    }
-  }
-
-  optionMenu.addEventListener("click", RPSUI);
-}
-
-
-
-//  Start-game UI to game UI
-
+const CHOICELIST = ["rock", "paper", "scissor"];
 const startGame = document.querySelector(".start-game-button")
 const roundMax= document.querySelector(".user-input-type")
-
-
-// when the button is clicked 
-// it removes the div and replace it with the game one
 
 function playerInScoreBoard(node, name, uniqueClass){
    const player1 = document.createElement("div")
@@ -157,7 +36,7 @@ function createCurrentRound(node){
     currentRound.classList.add("current-round")
 
     const player1Choice = document.createElement("div")
-    player1Choice.classList.add("player-one", "choice")
+    player1Choice.classList.add("player-one")
     choice(player1Choice,"paper","Y")
 
     const round = document.createElement("div")
@@ -175,7 +54,7 @@ function createCurrentRound(node){
 
 
     const player2Choice = document.createElement("div")
-    player2Choice.classList.add("player-two", "choice")
+    player2Choice.classList.add("player-two")
     choice(player2Choice,"rock","W")
     
   currentRound.appendChild(player1Choice)
@@ -204,7 +83,7 @@ function option(node,name,color,element="button"){
    const optionElement = document.createElement(element)
    let classAdd = ["option"]
     if (element === "div"){
-      classAdd.push("choice")
+      classAdd = ["choice"]
     }
     classAdd.forEach((cls) => optionElement.classList.add(cls))
     optionElement.classList.add(name)
@@ -239,10 +118,43 @@ function createUI(){
   screenGame.prepend(game) 
 }
 
+function getComputerChoice() {
+  let choice = Math.floor(Math.random() * 3)
+  return CHOICELIST[choice]
+}
 
-startGame.addEventListener("click",()=>{
-  document.querySelector(".start-game-section").style.display = "none"
-  createUI()
-})
-RPSuiGame(4)
-  
+function getHumanChoice() {
+  let choice = prompt("Choose to play(rock/paper/scissor): ")
+  return CHOICELIST[choice]
+}
+
+function RPSwinner(player1, player2) {
+  let playerI = CHOICELIST.indexOf(player1);
+  let playerII = CHOICELIST.indexOf(player2);
+  let diff = Math.abs(playerII - playerI);
+
+  if (diff == 2) {
+    return playerI > playerII ? 2 : 1;
+  } else if (diff == 1) {
+    return playerI > playerII ? 1 : 2;
+  } else if (diff == 0) {
+    return 0;
+  }
+}
+
+function isOption(e){
+  return CHOICELIST.reduce((a,c)=>a||e.target.classList.contains(c),false)
+}
+
+function choiceUpdate(playerNum,choice,color){
+  const playerChoice = document.querySelector(`.current-round .player-${playerNum} .choice img`)
+  playerChoice.src = `../Images/${color} ${choice}.png`
+}
+
+startGame.addEventListener("click", () => {
+  document.querySelector(".start-game-section").style.display = "none";
+  createUI();
+
+  choiceUpdate("one", "scissor", "W");
+  choiceUpdate("two", "paper", "Y");
+});
