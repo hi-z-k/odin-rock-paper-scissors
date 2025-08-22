4237a2545ab8cc706dc53cc3de3754fb444e13db
Hizkeal <104999321+hi-z-k@users.noreply.github.com.>
2025-08-22 03:06:07 +0300

    Partial Refactor the RPS game - UI interactivity
    - user can play the game by clicking the buttons
    - Winner is declared in each round
    - features not integrated to UI
        - user can enter the name they want
        - user can pick color(Yellow, White)

--- JS/app.js
+++ JS/app.js
@@ -2,12 +2,18 @@ const CHOICELIST = ["rock", "paper", "scissor"];
 const startGame = document.querySelector(".start-game-button")
 const roundMax= document.querySelector(".user-input-type")
 
-function playerInScoreBoard(node, name, uniqueClass){
+function playerInScoreBoard(node, name, color, uniqueClass){
    const player1 = document.createElement("div")
       player1.classList.add("player",uniqueClass)
 
       const player1Name = document.createElement("div")
         player1Name.classList.add("player-name")
+        if (color == "Y"){
+          player1Name.style.cssText = "background-color:yellow;"
+        }else{
+          player1Name.style.cssText = "background-color:white;"
+        }
+        
         player1Name.textContent = name
       player1.appendChild(player1Name)
 
@@ -17,27 +23,30 @@ function playerInScoreBoard(node, name, uniqueClass){
       player1.appendChild(player1Score)
     node.appendChild(player1)
 }
-function createScoreBoard(node){
+function createScoreBoard(node,playerName, player1Color){
     const scoreBoard = document.createElement("div")
       scoreBoard.classList.add("score-board")
-        playerInScoreBoard(scoreBoard,"p1","player-one")
+        playerInScoreBoard(scoreBoard,playerName,player1Color,"player-one")
         
         const vsSign = document.createElement("div")
           vsSign.classList.add("dash")
           vsSign.textContent = "—"
         scoreBoard.appendChild(vsSign)
-        
-        playerInScoreBoard(scoreBoard,"p2","player-two")
+        let player2Color = "W"
+        if (player1Color == "W"){
+          player2Color = "Y"
+        }
+        playerInScoreBoard(scoreBoard,"Computer",player2Color,"player-two")
       node.append(scoreBoard)
 }
 
-function createCurrentRound(node){
+function createCurrentRound(node, color){
   const currentRound = document.createElement("div")
     currentRound.classList.add("current-round")
 
     const player1Choice = document.createElement("div")
     player1Choice.classList.add("player-one")
-    choice(player1Choice,"paper","Y")
+    choice(player1Choice,"paper",color)
 
     const round = document.createElement("div")
       round.classList.add("round")
@@ -52,10 +61,14 @@ function createCurrentRound(node){
     round.appendChild(roundCount)
     round.appendChild(verses)
 
-
+    if (color == "W"){
+      color = "Y"
+    }else{
+      color = "W"
+    }
     const player2Choice = document.createElement("div")
     player2Choice.classList.add("player-two")
-    choice(player2Choice,"rock","W")
+    choice(player2Choice,"rock",color)
     
   currentRound.appendChild(player1Choice)
   currentRound.appendChild(round)
@@ -81,12 +94,11 @@ function choice(node,name,color){
 
 function option(node,name,color,element="button"){
    const optionElement = document.createElement(element)
-   let classAdd = ["option"]
+   let classAdd = "option"
     if (element === "div"){
-      classAdd = ["choice"]
+      classAdd = "choice"
     }
-    classAdd.forEach((cls) => optionElement.classList.add(cls))
-    optionElement.classList.add(name)
+    optionElement.classList.add(name,classAdd)
       const capitalizedName = name[0].toUpperCase()+name.slice(1)
       const imageIcon = document.createElement("img")
         imageIcon.classList.add("option-icon", name)
@@ -103,15 +115,15 @@ function option(node,name,color,element="button"){
 }
 
 
-function createUI(){
+function createUI(playerName, optionColor){
   const game = document.createElement("div")
     game.classList.add("game-section")
-    createScoreBoard(game)
+    createScoreBoard(game, playerName, optionColor)
     const mainSection = document.createElement("div")
     mainSection.classList.add("main-section")
 
-    createCurrentRound(mainSection)
-    createOptionSection(mainSection,"Y")
+    createCurrentRound(mainSection,optionColor)
+    createOptionSection(mainSection,optionColor)
     game.appendChild(mainSection)
 
   const screenGame = document.querySelector(".game-tabs")
@@ -123,10 +135,10 @@ function getComputerChoice() {
   return CHOICELIST[choice]
 }
 
-function getHumanChoice() {
-  let choice = prompt("Choose to play(rock/paper/scissor): ")
-  return CHOICELIST[choice]
-}
+// function getHumanChoice() {
+//   let choice = parseInt(prompt("Choose to play(rock-1/paper-2/scissor-3): "))
+//   return CHOICELIST[choice-1]
+// }
 
 function RPSwinner(player1, player2) {
   let playerI = CHOICELIST.indexOf(player1);
@@ -145,16 +157,48 @@ function RPSwinner(player1, player2) {
 function isOption(e){
   return CHOICELIST.reduce((a,c)=>a||e.target.classList.contains(c),false)
 }
+function optionOf(e){
+  let choicePicked = CHOICELIST.filter((c)=>e.target.classList.contains(c))
+  if (choicePicked.length == 1) return choicePicked[0]
+}
 
 function choiceUpdate(playerNum,choice,color){
   const playerChoice = document.querySelector(`.current-round .player-${playerNum} .choice img`)
   playerChoice.src = `../Images/${color} ${choice}.png`
 }
 
+function declareWinner(p1,p2){
+  let winner = RPSwinner(p1,p2)
+  const verses = document.querySelector(".current-round .round .verses")
+  if (winner) verses.textContent = `Player ${winner} WINS`
+  else verses.textContent = `It is a TIE`
+  return winner
+}
+
 startGame.addEventListener("click", () => {
   document.querySelector(".start-game-section").style.display = "none";
-  createUI();
+  
+  // who can reach the score first
+  // the score gets updated - takes in player and num and takes care of adding and updating
+  // rounds just get counted - same as score
+  createUI("Hizkeal","W");
 
-  choiceUpdate("one", "scissor", "W");
-  choiceUpdate("two", "paper", "Y");
+  const playerOptions = document.querySelector(".player-option")
+  playerOptions.addEventListener("click",(e)=>{
+    let p1Choice = optionOf(e)
+    if (!p1Choice) return
+    let p2Choice = getComputerChoice()
+
+    choiceUpdate("one",p1Choice,"W")
+    choiceUpdate("two", p2Choice, "Y")
+    
+    let winner = declareWinner(p1Choice,p2Choice)
+    
+    })
+
 });
+
+// return the verses text back to Vs after some timeout
+// radio buttons as an option for button type
+// using custom names for user
+// watch the web dev bootcamp on events
