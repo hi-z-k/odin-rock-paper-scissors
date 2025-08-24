23b8cccdd2b0837bbaf4901b390e8459582ccc9f
Hizkeal <104999321+hi-z-k@users.noreply.github.com.>
2025-08-24 10:40:43 +0300

    Image link adjustments

--- JS/app.js
+++ JS/app.js
@@ -95,7 +95,7 @@ function option(node, name, color, element = "button") {
   const capitalizedName = name[0].toUpperCase() + name.slice(1);
   const imageIcon = document.createElement("img");
   imageIcon.classList.add("option-icon", name);
-  imageIcon.src = `../Images/${color} ${name}.png`;
+  imageIcon.src = `/Images/${color} ${name}.png`;
   imageIcon.alt = capitalizedName;
   optionElement.prepend(imageIcon);
   if (element === "button") {
